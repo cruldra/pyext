4691483874af61973a74ea4bc6430c62fb6d12ef
cruldra <cruldra@gmail.com>
2024-09-03 03:37:29 +0800

    🎉 feat: 更新应用名称为“AI智剪工作流”并添加新文件 all_in_one_pro.py
    ✨ feat: 增加获取当前系统主题的功能
    
    📦 build: 更新 PyInstaller 配置以支持新依赖项

--- win.py
+++ win.py
@@ -1,9 +1,40 @@
+import winreg
+
 import win32gui
 import wrapt
 from clicknium import clicknium as cc, ui
 from tenacity import retry, stop_after_delay, wait_fixed
 
 from pyext.commons import ContextLogger
+
+
+def get_windows_theme():
+    """
+    获取当前系统使用的主题
+
+    仅限win11
+
+    Returns:
+        str: 主题类型,Dark Theme或Light Theme
+
+    Raises:
+        WindowsError: 如果无法确定主题类型,则引发此异常
+    """
+    # 打开注册表键
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
+                         r"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize")
+
+    # 读取 AppsUseLightTheme 值
+    value, _ = winreg.QueryValueEx(key, "AppsUseLightTheme")
+
+    # 关闭注册表键
+    winreg.CloseKey(key)
+
+    # 根据值返回主题类型
+    if value == 0:
+        return "Dark Theme"
+    else:
+        return "Light Theme"
 
 
 def is_window_active(window_title):
@@ -25,6 +56,7 @@ def is_window_active(window_title):
     # 比较窗口标题
     return active_title == window_title
 
+
 def wait_win(locator, timeout=0, interval=1):
     """
     这个装饰器会等待定位符(locator)指向的窗口出现并处于活动状态
@@ -36,7 +68,7 @@ def wait_win(locator, timeout=0, interval=1):
     window_name = str(locator).split(".")[-1]
     ContextLogger.set_name("win")
 
-    @retry(stop=stop_after_delay(timeout if timeout > 0 else 86400 )  , wait=wait_fixed(interval))
+    @retry(stop=stop_after_delay(timeout if timeout > 0 else 86400), wait=wait_fixed(interval))
     def wait_window_exists():
         """
         等待窗口出现
@@ -46,7 +78,7 @@ def wait_win(locator, timeout=0, interval=1):
             raise Exception(f"窗口[{window_name}]未打开")
         return True
 
-    @retry(stop=stop_after_delay(timeout if timeout > 0 else 86400) , wait=wait_fixed(interval))
+    @retry(stop=stop_after_delay(timeout if timeout > 0 else 86400), wait=wait_fixed(interval))
     def wait_window_active():
         """
         等待窗口处于活动状态
