7a77b20c212b2f00b758e1f961abdff18ba1dab6
cruldra <cruldra@gmail.com>
2024-09-23 09:59:24 +0800

    🎉 feat: 添加关闭多余剪映窗口的功能并引入emoji库

--- commons.py
+++ commons.py
@@ -1008,3 +1008,15 @@ class Json:
         将对象转换为JSON字符串
         """
         return json.dumps(obj, ensure_ascii=False, cls=Json.Encoder)
+
+
+def remove_emojis(text):
+    emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
